02af1d5f4854ee3085595d380d85465f8be3c9e8
kollyull <kollyull@163.com>
2019-11-12 09:09:42 +0800

    Add first.py

---
+++ first.py
@@ -0,0 +1,17 @@
+# name = input()
+# print("Hello,world!")
+# print(f"The input is {name}!")
+
+x=int(input())
+
+if x>0:
+    att = "positive"
+elif x<0:
+    att = "negative"
+else:
+    att = "zero"
+print(f"X is {att}.")
+
+
+coordinates = (10.0,20.0)
+names = ["Alice","Bob","Charlie"]
